05e7475c45359fb824fec1e049e75e300ed9ef81
K4am <krzyczkowski.wiktor00@gmail.com>
2022-11-29 09:08:07 +0100

    funkcje

--- zad1/functions.js
+++ zad1/functions.js
@@ -1,7 +1,14 @@
 function isOdd(x){
-    if(x%2==0)return false;
-    return true;
+    if(Number.isInteger(x))return x%2!=0;
+    return undefined;
+    
+
 }
 const isEven=(x)=>{
-    return !isOdd(x);
+    if(Number.isInteger(x))return x%2==0; 
+    return undefined;
 }
+console.log(isOdd(5));
+console.log(isEven(1));
+console.log(isOdd(null));
+console.log(isEven(undefined));
